18016d9e37185c63624db3cc65aea07bd09ffc1b
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2022-06-12 13:29:09 -0500

    fix: E05 instructions order

--- Exercises/E05/E05.gd
+++ Exercises/E05/E05.gd
@@ -1,12 +1,12 @@
 extends Node
 
 # 1. Create an array with your favorite animals
-# 2. Shuffle the array
-# 3. Print your top 1 animal using .find()
-# 4. Print the number of favorite animals
-# 5. Print if the array has "dog" in it
-# 6. Print the first animal without using indexes
-# 7. Print the last animal without using indexes
+# 2. Print the first animal without using indexes
+# 3. Print the last animal without using indexes
+# 4. Shuffle the array
+# 5. Print your top 1 animal using .find()
+# 6. Print the number of favorite animals
+# 7. Print if the array has "dog" in it
 
 func _ready():
 	pass # Replace with function body.
