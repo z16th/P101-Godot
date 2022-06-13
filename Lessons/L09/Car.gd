bbeaf2f0e708dd00fdebbc268d0a0d61ebd73f1b
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2022-06-13 01:16:47 -0500

    add: L09

---
+++ Lessons/L09/Car.gd
@@ -0,0 +1,34 @@
+extends Node
+class_name Car
+
+export var accel:float = 2.0
+export var friction:float = 0.1
+export var fuel_consumption:float = 1
+
+var fuel:float = 0
+var speed:float = 0
+
+func accel(delta):
+	if fuel > 0:
+		fuel -= fuel_consumption * delta
+		speed += accel * delta
+
+func deaccel(delta):
+	if speed > 0:
+		speed -= friction * delta
+	if speed <= 0:
+		speed = 0
+
+func unfair_accel():
+	if fuel > 0:
+		fuel -= fuel_consumption
+		speed += accel
+
+func unfair_deaccel():
+	if speed > 0:
+		speed -= friction
+	if speed <= 0:
+		speed = 0
+
+func refuel():
+	fuel += 10000
