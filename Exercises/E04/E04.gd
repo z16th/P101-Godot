72ecb9a0cfa9adaccfe6878fe5e37e638b005226
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2022-06-13 01:20:12 -0500

    fix: E04 node ref error

--- Exercises/E04/E04.gd
+++ Exercises/E04/E04.gd
@@ -2,21 +2,21 @@ extends Node
 
 #Use String methods to change the texts in the labels
 
-#1. Text in $Uppercase has uppercase format
-#2. Text in $Lowercase has lowercase format
-#3. Text in $Capitalized has capitalized format
-#4. Text in $Length displays the length of the phrase
-#5. Text in $StartsWithC displays if the phrase starts with "C"
-#6. Text in $EndsWithY displays if the phrase ends with "y"
-#7. Text in $HowManyEs displays the number of "e"s in the phrase
+#1. Text in $GridContainer/Uppercase has uppercase format
+#2. Text in $GridContainer/Lowercase has lowercase format
+#3. Text in $GridContainer/Capitalized has capitalized format
+#4. Text in $GridContainer/Length displays the length of the phrase
+#5. Text in $GridContainer/StartsWithC displays if the phrase starts with "C"
+#6. Text in $GridContainer/EndsWithY displays if the phrase ends with "y"
+#7. Text in $GridContainer/HowManyEs displays the number of "e"s in the phrase
 
 var phrase = "Captain Teemo on duty!"
 
 func _ready():
-	$Uppercase.text = "\n" #Replace this value
-	$Lowercase.text = "\n" #Replace this value
-	$Capitalized.text = "\n" #Replace this value
-	$Length.text = "\n" #Replace this value
-	$StartsWithC.text = "\n" #Replace this value
-	$EndsWithY.text = "\n" #Replace this value
-	$HowManyEs.text = "\n" #Replace this value
+	$GridContainer/Uppercase.text = "\n" #Replace this value
+	$GridContainer/Lowercase.text = "\n" #Replace this value
+	$GridContainer/Capitalized.text = "\n" #Replace this value
+	$GridContainer/Length.text = "\n" #Replace this value
+	$GridContainer/StartsWithC.text = "\n" #Replace this value
+	$GridContainer/EndsWithY.text = "\n" #Replace this value
+	$GridContainer/HowManyEs.text = "\n" #Replace this value
