f8d7f90e4c44109f7bce97ef4d8f80aecd3a8041
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2022-06-16 18:09:51 -0500

    add: E10

---
+++ Exercises/E10/E10.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# 1. Create a label that says "Hello"
+# 2. Create a LineEdit to grab the name from the player
+# 3. Create a sprite with any image you like
+# 4. Look for a sprite sequence on the internet an use it for an AnimatedSprite
+# 5. Grab a sound and make a button to play it
+
+
+func _ready():
+	pass
