5ec7e4c5e02692659fcc00f6183b548a30cc9c66
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2022-06-17 15:03:59 -0500

    refactor: E10

--- Exercises/E10/E10.gd
+++ Exercises/E10/E10.gd
@@ -1,11 +1,14 @@
 extends Node
 
-# 1. Create a label that says "Hello"
-# 2. Create a LineEdit to grab the name from the player
-# 3. Create a sprite with any image you like
-# 4. Look for a sprite sequence on the internet an use it for an AnimatedSprite
-# 5. Grab a sound and make a button to play it
+var username = "Player 1"
+var score = 12340
 
+# 1. Create a label that says "{username}: {score}"
+# 2. Create a LineEdit and set the placeholder value to "Type here to chat..."
+# 3. Create a sprite with any image you like
+# 4. Use the images inside the assets folder and use them to make an AnimatedSprite
+# 5. Create an AudioStreamPlayer and link it to any sound from the assets folder
+# 6. Create a button and connect the pressed() signal using this script (E10) and use it to play the sound from #5
 
 func _ready():
 	pass
