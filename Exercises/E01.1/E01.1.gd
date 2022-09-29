efed14e909abfe1a560b8a7a3e16ed295275a456
Elihu Munguía <47308963+z16th@users.noreply.github.com>
2022-09-29 01:22:05 -0500

    Revert "Merge branch 'dev'"
    This reverts commit 542bc33bf92ad5b515be0dcc15b0123eea2b9a53, reversing
    changes made to a94417c5bd8cc30aefd2c9745ec1ca1c2ec12d84.

--- Exercises/E01.1/E01.1.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# 0. Look for "weakly type" and "strongly typed" meanings
-# 1. Declare and print a weakly typed variable of each type: int, float, bool, String, Array, Dictionary
-# 2. Declare and print a strongly typed variable of each type: int, float, bool, String, Array, Dictionary
-# 3. Declare and print three different constants
-
-func _ready():
-	pass
--- Playground.gd
+++ Playground.gd
@@ -1,17 +1,5 @@
 extends Node
 
-var message = "this is a MESSAGE"
 
 func _ready():
-	print(message.capitalize())
-	print(message.to_upper())
-	print(message.to_lower())
-	print(message.length())
-	print(message.begins_with("this"))
-	print(message.ends_with("message"))
-	print(message.find("is a"))
-	print(message.replace("MESSAGE","NOTICE"))
-	
-	print("16.0".to_float())
-	print("16".to_int())
-	
+	print( "Another string".length() )
